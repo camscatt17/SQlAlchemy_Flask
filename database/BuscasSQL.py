01c481632608f148fc5440e4ef81356baad34e3f
camscatt17 <camscatt17@gmail.com>
2023-05-18 18:52:13 -0300

    Buscas SQL.

---
+++ database/BuscasSQL.py
@@ -0,0 +1,15 @@
+from sqlalchemy import text, and_
+from __init__ import session, Base
+from Product import Product
+
+## Retorno de todos os produtos
+# Primeira forma
+product = session.query(Product).from_statement(text("SELECT * FROM product")).all()
+print(product)
+
+# Segunda forma
+product = session.query(Product).all()
+print(product)
+
+product = session.query(Product).filter(and_(Product.price > 50, Product.price < 1000)).all()
+print(product)
